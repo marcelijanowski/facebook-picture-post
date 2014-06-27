bbb238122e35da5be9607edb6d40e55e46139f07
Marceli Janowski <marceli@mjdesign.pl>
2014-06-27 20:12:23 +0100

    Add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function (grunt) {
+  require('time-grunt')(grunt);
+
+  grunt.loadTasks('grunt');
+
+  grunt.registerTask('default', 'run the server and watch for changes', [
+    'express'
+  ]);
+});
