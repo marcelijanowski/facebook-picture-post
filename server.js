ca62040bcfd9dcb6766a4852cef89ae42ef5c304
Marceli Janowski <marceli@mjdesign.pl>
2014-06-27 20:18:00 +0100

    Small changes to server.js file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.configure(function () {
     response.header('Access-Control-Allow-Origin', '*');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header(
-      'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+      'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length'
     );
 
     next();
