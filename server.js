3388e49dcdc151bb535268cfed6df256d123ea1a
Marceli Janowski <marceli@mjdesign.pl>
2014-06-27 20:11:24 +0100

    Add basic html + express server

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    req = require('request'),
+    path = require('path'),
+    app,
+    port;
+
+app = express();
+port = process.env.PORT || 8000;
+req = require('request');
+
+process.env.PWD = process.cwd();
+
+app.configure(function () {
+  app.use(function (request, response, next) {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    response.header(
+      'Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key'
+    );
+
+    next();
+  });
+
+  app.use('/', express.static(__dirname + '/'));
+});
+
+app.options('*', function (request, response, next) {
+  response.send(200);
+});
+
+app.all('*', function (request, response, next) {
+  response.sendfile(__dirname + '/index.html');
+});
+
+app.listen(port, function () {
+  console.log('Listening on port: ' + port);
+});
